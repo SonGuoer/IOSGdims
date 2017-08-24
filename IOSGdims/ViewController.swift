8ade5ce3d60280f2b67884fff06a298ae515db1b
qingsong <1154046205@qq.com>
2017-08-24 17:54:49 +0800

    Initial Commit

---
+++ IOSGdims/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSGdims
+//
+//  Created by 李青松 on 2017/8/24.
+//  Copyright © 2017年 name. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
