f29f6fdce53708203530dd242544d2a99ebbcff3
baohongyan <657175417@qq.com>
2017-08-29 11:16:42 +0800

    ViewController回收键盘

--- IOSGdims/ViewController.swift
+++ IOSGdims/ViewController.swift
@@ -10,17 +10,29 @@ import UIKit
 import Alamofire
 import HandyJSON
 import SwiftyJSON
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var text: UITextField!
     @IBOutlet weak var btn: UIButton!
+    
+    @IBOutlet weak var ipText: UITextField!
+    @IBOutlet weak var portText: UITextField!
+    @IBOutlet weak var phoneNumText: UITextField!
+    @IBOutlet weak var personType: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        ipText.delegate = self
+        portText.delegate = self
+        
     }
-   
     
     @IBAction func touchupInsidePostRequestBtnAction(_ sender: AnyObject) {
+        //获取人员类型的值
+        let typeNumber = personType.selectedSegmentIndex
+        print("\(typeNumber)")
+        
         let parameters = [
             "mobile": "15702323457",
             "imei": "0"
@@ -56,7 +68,7 @@ class ViewController: UIViewController {
                 case .failure(let error):
                     print(error)
                 }
-                
+        
         }
         
         
@@ -68,6 +80,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    /*
+     回收系统键盘
+     */
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    /*
+     点击空白处收起keyboard
+     */
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        phoneNumText.resignFirstResponder()
+    }
+    
 }
 
